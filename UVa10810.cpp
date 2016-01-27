792b3c03e055c6c85a76412eead3ce02f8442a67
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-01-27 10:34:18 +0800

    n^2 -> nlogn UVa10810

--- UVa10810.cpp
+++ UVa10810.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
-
+#define sum 500000
 using namespace std;
-
+/*
 void swapx(long long &a,long long &b)
 {
     long long temp = a;
     a = b;
     b = temp;
 }
-
+*/
+long long total;
+long long buf[sum];
+void merge(int l,int r,long long arr[])
+{
+    if(l == r)
+        return;
+    int m = (r + l) / 2;
+    merge(l, m, arr);
+    merge(m + 1, r, arr);
+    int lptr = l, rptr = m + 1, ptr = l;
+    while(lptr < m + 1 || rptr < r + 1)
+    {
+        while(lptr < m + 1 && rptr < r + 1)
+        {
+            if(arr[lptr] > arr[rptr])
+            {
+                buf[ptr] = arr[rptr];
+                rptr++;     ptr++;
+                if(rptr - ptr > 0)
+                    total += rptr - ptr;
+            }
+            else
+            {
+                buf[ptr] = arr[lptr];
+                lptr++;     ptr++;
+                if(lptr - ptr > 0)
+                    total += lptr - ptr;
+            }
+        }
+        if(lptr < m + 1)
+        {
+            buf[ptr] = arr[lptr];
+            lptr++;     ptr++;
+            if(lptr - ptr > 0)
+                total += lptr - ptr;
+        }
+        else
+        {
+            buf[ptr] = arr[rptr];
+            rptr++;     ptr++;
+            if(rptr - ptr > 0)
+                total += rptr - ptr;
+        }
+    }
+    for(int i = l;i <= r;i++)
+    {
+        arr[i] = buf[i];
+    }
+}
 int main()
 {
     int n;
@@ -17,24 +66,17 @@ int main()
         if(!n)
             break;
 
-        int total = 0;
+        total = 0;
+        //int total = 0;
         long long arr[n];
         for(int i = 0;i < n;i++)
         {
             cin >> arr[i];
         }
+        merge(0, n - 1,arr);
         for(int i = 0;i < n;i++)
-        {
-            for(int j = i + 1;j < n;j++)
-            {
-                if(arr[i] > arr[j])
-                {
-                    swapx(arr[i],arr[j]);
-                    total++;
-                }
-            }
-        }
-        cout << total << endl;
+            cout << buf[i] << " ";
+          cout << total << endl;
     }
     return 0;
 }
