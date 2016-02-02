77ce81c710d574c1acaa8981fc0ca5573ef08024
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-02-02 09:11:01 +0800

    add toj

---
+++ TOJ/249.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define maxn 500000
+
+using namespace std;
+
+int main()
+{
+    int arr[maxn],sum = 0,n,ans = 0;
+    cin >> n;
+    for(int i = 0;i < n;i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 0;i < n;i++)
+    {
+        if(sum + arr[i] > 0)
+        {
+            sum = sum + arr[i];
+            if(sum > ans)
+            {
+                ans = sum;
+            }
+        }
+        else
+        {
+            sum = 0;
+        }
+    }
+    cout << ans << endl;
+}
