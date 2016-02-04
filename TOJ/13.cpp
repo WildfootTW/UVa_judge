5b59860bf65601ff63961a2b481cf0dfe65b05fb
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-02-04 11:28:03 +0800

    improve 13.cpp

--- TOJ/13.cpp
+++ TOJ/13.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define INF 2147483647
+#include <cstdio>
 //#define MAX 100000    //MAX10000000
 
 using namespace std;
@@ -12,43 +13,44 @@ int main()
     {
         int ans = 0, n;
         cin >> n;
-        int item[n][2], dp[2][V + 1];
+        int item[n][2], dp[V + 1];
         for(int i = 0;i < n;i++)
             for(int j = 0;j < 2;j++)
-                cin >> item[i][j];
+                scanf("%d", &item[i][j]);
+                //cin >> item[i][j];
         
-        for(int i = 0;i <= V;i++)   dp[0][i] = -INF;
-        dp[0][0] = 0;
-        dp[0][item[0][0]] = item[0][1]; 
+        for(int i = 0;i <= V;i++)   dp[i] = -INF;
+        dp[0] = 0;
+        dp[item[0][0]] = item[0][1]; 
 
         for(int i = 0;i < n;i++)
         {
 
-            int temp = V / item[i][0];
+            //int temp = V / item[i][0];
             //cout << "V = " << V << " item[i][0] = " << item[i][0] << endl;
             //cout << "temp[" << i <<"] = " << temp << endl;
-            for(int k = 0;k < temp;k++)
-            {
+            //for(int k = 0;k < temp;k++)
+            //{
                 for(int j = 0;j <= V;j++)
                 {
                     if(j < item[i][0])
-                        dp[1][j] = dp[0][j];
+                        dp[j] = dp[j];
                     else
                     {
-                        dp[1][j] = dp[0][j - item[i][0]] + item[i][1];
-                        if(dp[1][j] < dp[0][j])
-                            dp[1][j] = dp[0][j];
+                        dp[j] = dp[j - item[i][0]] + item[i][1];
+                        if(dp[j] < dp[j])
+                            dp[j] = dp[j];
                     }
                 }
-                for(int j = 0;j <= V;j++)
-                {
-                    swap(dp[1][j],dp[0][j]);
-                }
-            }
+                //for(int j = 0;j <= V;j++)
+                //{
+                //    swap(dp[1][j],dp[0][j]);
+                //}
+            //}
         }
         for(int i = 0;i <= V;i++)
-            if(ans < dp[0][i])
-                ans = dp[0][i];
+            if(ans < dp[i])
+                ans = dp[i];
 
         /*
         cout << "count = " << count << endl;    
