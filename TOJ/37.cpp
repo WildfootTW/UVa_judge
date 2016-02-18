23f7f0f51df570e775b833a59c72bb16e846639e
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-02-18 09:31:52 +0800

    add 37

---
+++ TOJ/37.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    while(getline(cin,s))
+    {
+        if(s == "\0")
+            break;
+        int total = 0;
+        for(int i = 0;i < s.length();i++)
+        {
+            if(s[i] == ' ')
+                total++;
+        }
+        cout << total << endl;
+    }
+
+
+
+    return 0;
+}
