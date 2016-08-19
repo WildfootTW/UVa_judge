b65f9b2e27fa2bc5a290767ae9e262a16ce2c980
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-08-19 15:46:30 +0800

    yo

---
+++ TOJ/241.cpp
@@ -0,0 +1,26 @@
+//dp[][][][] 不想做
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    //========== Keyin ==========
+    int n;
+    cin >> n;
+    int b[n];
+    for(int i = 0;i < n;i++)
+        cin >> b[i];
+    int m;
+    cin >> m;
+    int wv[m][2];
+    for(int i = 0;i < m;i++)
+    {
+        cin >> wv[i][0] >> wv[i][1];
+    }
+
+    //==========  ==========
+
+    
+}
