440c679d1b4ad7c48dcd2138d5170b9368cf7dc2
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-03-07 19:37:59 +0800

    complete Flip Sort

---
+++ UVa/UVa10327.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <utility>
+#define MAX 1005
+
+using namespace std;
+
+int Flip_Sort(int N)
+{
+    int arr[MAX], num = 0;
+    for(int i = 0;i < N;i++)
+        cin >> arr[i];
+
+    for(int i = 0;i < N - 1;i++)
+    {
+        for(int j = 0;j < N - 1 - i;j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                num++;
+            }
+        }
+    }
+
+//    for(int i = 0;i < N;i++)
+//        cout << arr[i] << " ";
+//    cout << endl;
+
+    return num;
+}
+
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        cout << "Minimum exchange operations : " << Flip_Sort(n) << endl;
+    }
+    return 0;   
+}
+
