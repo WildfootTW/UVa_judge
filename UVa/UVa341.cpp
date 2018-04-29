6424cda6b124421901cff152319cb63337d894b5
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-30 00:29:37 +0800

    update UVa341

--- UVa/UVa341.cpp
+++ UVa/UVa341.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <ctime>
 #include <vector>
-#include <functional>
+#include <queue>
 
 #define INF 2147483647
 #define EPS 1e-9
@@ -23,24 +23,42 @@ using namespace std;
 class edge
 {
     public:
-        int value, to_node;
-        edge(void);
-        edge(int input_to, int input_value);
-        bool operator > (const edge&);
-}
-edge::edge()
+        int delay, to_node, from_node;
+        edge(int input_from, int input_to, int input_delay);
+        bool operator> (const edge&);
+        bool operator< (const edge&);
+};
+class node
 {
-}
-edge::edge(int input_to, int input_value):
-value(input_value), to_node(input_to)
+    public:
+        int total_delay, pre;
+        vector<edge> edges;
+        node(int input_delay = INF);
+};
+edge::edge(int input_from, int input_to, int input_delay):
+from_node(input_from), delay(input_delay), to_node(input_to)
 {
 }
 bool edge::operator> (const edge& param)
 {
-    return this.value > param.value;
+    return this->delay > param.delay;
+}
+bool edge::operator< (const edge& param)
+{
+    return this->delay < param.delay;
+}
+node::node(int input_delay):
+total_delay(input_delay), pre(-1)
+{
 }
 
-vector<edge> intersection[MAX];
+struct custom_compare
+{
+    bool operator()(edge &lhs, edge &rhs)
+    {
+        return lhs.delay < rhs.delay;
+    }
+};
 
 int main()
 {
@@ -51,6 +69,10 @@ int main()
     {
         if(!node_num)
             break;
+
+        priority_queue< edge, vector<edge>, custom_compare > pq;
+        node * nodes = new node[node_num + 1];
+
         // input
         for(int i = 1;i <= node_num;i++)
         {
@@ -58,16 +80,29 @@ int main()
             cin >> k;
             for(int j = 0;j < k;j++)
             {
-                int input_node, input_value;
-                cin >> input_node >> input_value;
-                intersection[i].push_back(edge{input_node, input_value});
+                int input_to, input_delay;
+                cin >> input_to >> input_delay;
+                nodes[i].edges.push_back(edge{i, input_to, input_delay});
             }
         }
         int node_start, node_end;
         cin >> node_start >> node_end;
 
-        priority_queue< edge, vector<edge>, greater<edge> > pq;
-        pq.push(edge{node_start, 0});
+        nodes[node_start].total_delay = 0;
+        nodes[node_start].pre = -1;
+        for(edge n:nodes[node_start].edges)
+            pq.push(n);
+
+        while(pq.size())
+        {
+            node current_node = nodes[pq.top().to_node];
+            pq.pop();
+            for(edge next_edge:current_node.edges)
+            {
+                if(node[current_edge.to_node].total_delay + next_edge.delay > node[next_edge.to_node].total_delay )
+            }
+
+        }
     }
 
 
