d98d2384440140b59b9616e9a2d0f6e9e09aed21
WildfootW <wildfootw@wildfoo.tw>
2021-01-02 16:51:29 +0800

    UVa 1754: Pass example

--- UVa Online Judge/1754/UVa1754.cpp
+++ UVa Online Judge/1754/UVa1754.cpp
@@ -8,7 +8,7 @@
 // [ ] Completed
 
 #include <iostream>
-#include <array>
+#include <vector>
 #include <cmath>
 #include <algorithm>
 #include <ctime>
@@ -17,37 +17,45 @@ using namespace std;
 
 static const unsigned long long int INF = 1e18;
 
-template<size_t D, size_t K>
 class UVa1754
 {
 public:
-    UVa1754()
+    UVa1754(size_t total_d, size_t total_k):
+        total_d(total_d), total_k(total_k),
+        squared_errors(total_d, vector< vector<unsigned long long int> >
+                      (total_d + 1, vector<unsigned long long int>
+                      (total_k + 1, INF))), // squared_errors[D][D][K] = INF
+        pixels(total_d)             // pixels[D]
     {
-        for(size_t i = 0;i < D;++i)
-            for(size_t j = 0;j < D;++j)
-                squared_errors[i][j].fill(INF);
+
     }
-    unsigned long long int get_sum_squared_errors(size_t pos = 0, size_t d = D, size_t k = K)
+    unsigned long long int get_answer()
     {
-        if(squared_errors[pos][d][k] != INF) return squared_errors[pos][d][k];
-        if(k == 1) return squared_errors[pos][d][k] = get_squared_errors(pos, d);
-        unsigned long long int ret = INF;
-        for(size_t i = 1;i <= d;++i)
-        {
-            ret = min(ret, get_sum_squared_errors(pos, i, 1) + get_sum_squared_errors(i, k - i, k - 1));
-        }
-        return squared_errors[pos][d][k] = ret;
+        return get_sum_squared_errors(0, total_d, total_k);
     }
     void input()
     {
-        for(size_t i = 0;i < D;++i)
+        for(size_t i = 0;i < total_d;++i)
         {
             cin >> pixels[i].first >> pixels[i].second;
         }
     }
 private:
-    array<array<array<unsigned long long int, K>, D>, D> squared_errors; // array[D][D][K]
-    array<pair<int, unsigned long long int>, D> pixels;
+    size_t total_d, total_k;
+    vector< vector< vector<unsigned long long int> > > squared_errors;
+    vector< pair<int, unsigned long long int> > pixels;
+
+    unsigned long long int get_sum_squared_errors(size_t pos, size_t d, size_t k)
+    {
+        if(squared_errors[pos][d][k] != INF) return squared_errors[pos][d][k];
+        if(k == 1) return squared_errors[pos][d][k] = get_squared_errors(pos, d);
+        unsigned long long int ret = INF;
+        for(size_t i = 1;i <= (d - k + 1);++i) // first block d
+        {
+            ret = min(ret, get_sum_squared_errors(pos, i, 1) + get_sum_squared_errors(i, d - i, k - 1));
+        }
+        return squared_errors[pos][d][k] = ret;
+    }
 
     unsigned long long int get_squared_errors(size_t pos, size_t d)
     {
@@ -57,7 +65,7 @@ private:
             unsigned long long int nret = 0;
             for(size_t j = pos;j < (pos + d);++j)
             {
-                nret += (pow((pixels[pos + j].first - i), 2) * pixels[pos + j]);
+                nret += (pow((pixels[j].first - (int)i), 2) * pixels[j].second);
             }
             ret = min(ret, nret);
         }
@@ -70,9 +78,9 @@ int main()
     size_t d, k;
     while(cin >> d >> k)
     {
-        UVa1754<d, k> solver;
+        UVa1754 solver(d, k);
         solver.input();
-        unsigned long long int answer = solver.get_sum_squared_errors();
+        unsigned long long int answer = solver.get_answer();
         cout << answer << endl;
     }
 
